921aaa1fe224f0b0ac076f73a410232480fbe25c
Yash Agarwal <yash23.agarwal@gmail.com>
2022-06-21 15:38:49 +0530

    Hash generator for the various KVS to RC Migration state constants

---
+++ HashGenerator/BuiltInTypeSerializer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HashGenerator
+{
+    internal class BuiltInTypeSerializer<T>
+    {
+        /// <summary>
+        /// The binary reader method appropriate for type T
+        /// </summary>
+        private Func<BinaryReader, T> binaryReaderFunction;
+
+        /// <summary>
+        /// The binary writer method appropriate for type T
+        /// </summary>
+        private Action<BinaryWriter, T> binaryWriterFunction;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuiltInTypeSerializer{T}"/> class.
+        /// </summary>
+        /// <param name="binaryWriterFunction"> Binary Writer Function to use</param>
+        /// <param name="binaryReaderFunction"> Binary Reader Function to use</param>
+        internal BuiltInTypeSerializer(
+            Action<BinaryWriter, T> binaryWriterFunction,
+            Func<BinaryReader, T> binaryReaderFunction)
+        {
+            this.binaryWriterFunction = binaryWriterFunction;
+            this.binaryReaderFunction = binaryReaderFunction;
+        }
+
+        /// <summary>
+        /// Returns whether this class supports the provided type
+        /// </summary>
+        /// <returns>Whether the type parameter is supported</returns>
+        public static bool IsSupportedType()
+        {
+            var builtInType = typeof(T);
+            return builtInType.Equals(typeof(Guid)) || builtInType.Equals(typeof(bool))
+                   || builtInType.Equals(typeof(byte)) || builtInType.Equals(typeof(sbyte))
+                   || builtInType.Equals(typeof(char)) || builtInType.Equals(typeof(decimal))
+                   || builtInType.Equals(typeof(double)) || builtInType.Equals(typeof(float))
+                   || builtInType.Equals(typeof(int)) || builtInType.Equals(typeof(uint))
+                   || builtInType.Equals(typeof(long)) || builtInType.Equals(typeof(ulong))
+                   || builtInType.Equals(typeof(short)) || builtInType.Equals(typeof(ushort))
+                   || builtInType.Equals(typeof(string)) || builtInType.Equals(typeof(byte[]));
+        }
+
+        /// <summary>
+        /// From byte array.
+        /// </summary>
+        /// <param name="binaryReader"> Binary Reader instance </param>
+        /// <returns>Representation of value as a 'T'</returns>
+        public T Read(BinaryReader binaryReader)
+        {
+            return (T)this.binaryReaderFunction(binaryReader);
+        }
+
+        /// <summary>
+        /// De-serializes to object.
+        /// </summary>
+        /// <param name="baseValue"> Base value </param>
+        /// <param name="reader"> Instance of Binary Reader </param>
+        /// <returns>Deserialized version of T.</returns>
+        public T Read(T baseValue, BinaryReader reader)
+        {
+            return this.Read(reader);
+        }
+
+        /// <summary>
+        /// To byte array.
+        /// </summary>
+        /// <param name="value">Value to serialize.</param>
+        /// <param name="binaryWriter"> Binary Writer instance </param>
+        public void Write(T value, BinaryWriter binaryWriter)
+        {
+            this.binaryWriterFunction(binaryWriter, value);
+        }
+
+        /// <summary>
+        /// Serializes in to binary writer.
+        /// </summary>
+        /// <param name="currentValue"> Current value </param>
+        /// <param name="newValue"> New value</param>
+        /// <param name="binaryWriter"> Binary Writer Instance </param>
+        public void Write(T currentValue, T newValue, BinaryWriter binaryWriter)
+        {
+            this.Write(newValue, binaryWriter);
+        }
+    }
+}
---
+++ HashGenerator/Program.cs
@@ -0,0 +1,130 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace HashGenerator
+{
+    internal class Program
+    {
+        // private static DataContractStateSerializer<string> keyConverter = new DataContractStateSerializer<string>();
+        private static BuiltInTypeSerializer<string> keyConverter = BuiltInTypeSerializerBuilder.MakeBuiltInTypeSerializer<string>();
+
+        private static ArraySegment<byte> GetKeyBytes(string key)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var binaryWriter = new BinaryWriter(memoryStream))
+                {
+                    keyConverter.Write(key, binaryWriter);
+
+                    return new ArraySegment<byte>(memoryStream.GetBuffer(), 0, (int)memoryStream.Position);
+                }
+            }
+        }
+
+
+        private static List<string> GetArgValues(dynamic argDesc)
+        {
+            if (argDesc["Type"] == "string")
+            {
+                return argDesc["Value"].ToObject<List<string>>();
+            }
+            else if (argDesc["Type"] == "num")
+            {
+                List<string> nums = new List<string>();
+                for (int i = 0; i <= argDesc["Value"].ToObject<int>(); i++)
+                {
+                    nums.Add(i.ToString());
+                }
+                return nums;
+            }
+
+            return null;
+        }
+
+        static void Main(string[] args)
+        {
+            Dictionary<ulong, string> hashDict = new Dictionary<ulong, string>();
+
+            using (StreamReader r = new StreamReader("KeyValues.json"))
+            {
+                string json = r.ReadToEnd();
+                List<dynamic> keys = ((IEnumerable<dynamic>)JsonConvert.DeserializeObject<dynamic>(json)).ToList();
+
+                Console.WriteLine(keys.Count);
+
+                foreach (var key in keys)
+                {
+                    var argDesc = key.ArgDesc;
+                    int argNum = argDesc.Count;
+                    if (argNum == 0)
+                    {
+                        string keyString = key.KeyFormat;
+                        var keyBytes = GetKeyBytes(keyString);
+                        ulong keyHash = CRC64.ToCRC64(keyBytes);
+                        hashDict.Add(keyHash, keyString);
+                    }
+                    else if (argNum == 1)
+                    {
+                        List<string> argValues1 = GetArgValues(argDesc[0]);
+                        foreach (var argValue1 in argValues1)
+                        {
+                            string keyFormat = key.KeyFormat.ToObject<string>();
+                            string keyString = String.Format(keyFormat, argValue1);
+                            var keyBytes = GetKeyBytes(keyString);
+                            ulong keyHash = CRC64.ToCRC64(keyBytes);
+                            hashDict.Add(keyHash, keyString);
+                        }
+                    }
+                    else if (argNum == 2)
+                    {
+                        List<string> argValues1 = GetArgValues(argDesc[0]);
+                        List<string> argValues2 = GetArgValues(argDesc[1]);
+
+                        foreach (var argValue1 in argValues1)
+                        {
+                            foreach (var argValue2 in argValues2)
+                            {
+                                string keyFormat = key.KeyFormat.ToObject<string>();
+                                string keyString = String.Format(keyFormat, argValue1, argValue2);
+                                var keyBytes = GetKeyBytes(keyString);
+                                ulong keyHash = CRC64.ToCRC64(keyBytes);
+                                hashDict.Add(keyHash, keyString);
+                            }
+                        }
+                    }
+                    else if (argNum == 2)
+                    {
+                        List<string> argValues1 = GetArgValues(argDesc[0]);
+                        List<string> argValues2 = GetArgValues(argDesc[1]);
+                        List<string> argValues3 = GetArgValues(argDesc[2]);
+
+                        foreach (var argValue1 in argValues1)
+                        {
+                            foreach (var argValue2 in argValues2)
+                            {
+                                foreach (var argValue3 in argValues2)
+                                {
+                                    string keyFormat = key.KeyFormat.ToObject<string>();
+                                    string keyString = String.Format(keyFormat, argValue1, argValue2, argValue3);
+                                    var keyBytes = GetKeyBytes(keyString);
+                                    ulong keyHash = CRC64.ToCRC64(keyBytes);
+                                    hashDict.Add(keyHash, keyString);
+                                }
+                            }
+                        }
+                    }
+                }
+                //string hashDictJson = JsonConvert.SerializeObject(hashDict);
+                //File.AppendAllText("hashes.json", hashDictJson);
+
+                File.WriteAllLines(
+                        "hashes.dmp",
+                        hashDict.Select(kvp => string.Format("{0};{1}", kvp.Key, kvp.Value)));
+            }
+        }
+    }
+}
